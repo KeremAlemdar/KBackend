fbcf92ba24941dda14ac107f3dba5797d50fc850
Kerem Alemdar <59143416+KeremAlemdar@users.noreply.github.com>
2021-11-26 19:31:04 +0300

    Update RankController.cs

--- EmployeeProject/Controllers/RankController.cs
+++ EmployeeProject/Controllers/RankController.cs
@@ -1,20 +1,20 @@
-﻿//using EmployeeProject.Models;
-//using EmployeeProject.Repositories;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+using EmployeeProject.Models;
+using EmployeeProject.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace EmployeeProject.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class RankController : EmployeeProjectDBController<Rank, RankRepository>
-//    {
-//        public RankController(RankRepository repository) : base(repository)
-//        {
+namespace EmployeeProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankController : EmployeeProjectDBController<Rank, RankRepository>
+    {
+        public RankController(RankRepository repository) : base(repository)
+        {
 
-//        }
-//    }
-//}
+        }
+    }
+}
